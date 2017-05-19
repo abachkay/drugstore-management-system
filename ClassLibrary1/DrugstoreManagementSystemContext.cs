2b9dea9a2010c4a26b7d4bb40b571ec118eaa454
abachkay <abachkay@gmail.com>
2017-05-19 17:58:15 +0300

    Added UI project

---
+++ ClassLibrary1/DrugstoreManagementSystemContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+
+namespace DrugstoreManagementSystem.Entities
+{
+    public class DrugstoreManagementSystemContext:DbContext
+    {
+        public DrugstoreManagementSystemContext(): base("name=DrugstoreManagementSystem")
+        {
+        }
+        //public virtual DbSet<Car> Cars { get; set; }
+        //public virtual DbSet<Customer> Customers { get; set; }
+        //public virtual DbSet<Manager> Managers { get; set; }
+        //public virtual DbSet<Order> Orders { get; set; }
+    }
+}
